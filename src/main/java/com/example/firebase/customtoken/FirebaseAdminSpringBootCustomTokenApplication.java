4019949c9cab1701d5c6494a7db050246fe4e646
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2021-06-07 21:12:53 +0300

    ilk commit

---
+++ src/main/java/com/example/firebase/customtoken/FirebaseAdminSpringBootCustomTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.firebase.customtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseAdminSpringBootCustomTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirebaseAdminSpringBootCustomTokenApplication.class, args);
+    }
+
+}
